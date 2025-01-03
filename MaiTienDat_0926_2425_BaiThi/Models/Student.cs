aebc2f59783bb9ac39f73d27a4ba102343944b46
maitd <maitd2010@gmail.com>
2025-01-03 08:11:55 +0700

    CREATE_TABLE

---
+++ MaiTienDat_0926_2425_BaiThi/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace MaiTienDat_0926_2425_BaiThi.Models
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Age {  get; set; }
+    }
+}

fce4ca58ccbdad101171d69c562cad143111cd40
maitd <maitd2010@gmail.com>
2025-01-03 07:47:03 +0700

    Add project files.

---
+++ MaiTienDat_0926_2425_BaiThi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaiTienDat_0926_2425_BaiThi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

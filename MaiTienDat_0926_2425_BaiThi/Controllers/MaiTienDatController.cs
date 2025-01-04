13b7112970f3aa3deb31292e5497b99e8f321d2c
maitd <maitd2010@gmail.com>
2025-01-04 09:45:16 +0700

    update-migration db student

---
+++ MaiTienDat_0926_2425_BaiThi/Controllers/MaiTienDatController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaiTienDat_0926_2425_BaiThi.Controllers
+{
+    public class MaiTienDatController : Controller
+    {
+        public IActionResult Index()
+        {
+            const string str= "Mai Tien Dat";
+            return View(str);
+        }
+    }
+}
